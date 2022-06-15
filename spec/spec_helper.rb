e8c5cd6df653bf35e6e7b9e471ea29ed898378eb
Andrew W. Lee <git@drewlee.com>
2022-06-14 20:59:27 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,9 @@
-require 'rubygems'
-require 'bundler'
-Bundler.setup
-
-require 'rake'
-require 'rspec'
+require 'bundler/setup'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'rake'
 require 'active_support'
 require 'active_support/core_ext/object/blank'
 require 'active_support/core_ext/class/subclasses'
@@ -17,3 +13,17 @@ require 'annotate/parser'
 require 'annotate/helpers'
 require 'annotate/constants'
 require 'byebug'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  # config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  # config.order = :random
+end
