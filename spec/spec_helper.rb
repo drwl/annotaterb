2114a022cc549282cc1183614ab67115e6fee2ef
Andrew W. Lee <git@drewlee.com>
2022-06-14 21:36:23 -0700

    Remove unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,4 @@
 require 'bundler/setup'
-
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../lib'))
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
-
 require 'rake'
 require 'active_support'
 require 'active_support/core_ext/object/blank'
