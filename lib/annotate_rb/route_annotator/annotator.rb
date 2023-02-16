135d7e7db53b397106462f134f9490c907c37008
Andrew W. Lee <git@drewlee.com>
2023-02-16 14:25:25 -0800

    Use refactor from https://github.com/ctran/annotate_models/pull/793

--- lib/annotate_rb/route_annotator/annotator.rb
+++ lib/annotate_rb/route_annotator/annotator.rb
@@ -18,60 +18,17 @@ module AnnotateRb
       end
 
       def add_annotations
-        if Helper.routes_file_exist?
-          existing_text = File.read(Helper.routes_file)
-          content, header_position = Helper.strip_annotations(existing_text)
-          new_content = annotate_routes(HeaderGenerator.generate(@options), content, header_position, @options)
-          new_text = new_content.join("\n")
-
-          if Helper.rewrite_contents(existing_text, new_text)
-            puts "#{Helper.routes_file} was annotated."
-          else
-            puts "#{Helper.routes_file} was not changed."
-          end
-        else
-          puts "#{Helper.routes_file} could not be found."
+        routes_file = File.join('config', 'routes.rb')
+        AnnotationProcessor.execute(@options, routes_file).tap do |result|
+          puts result
         end
       end
 
       def remove_annotations
-        if Helper.routes_file_exist?
-          existing_text = File.read(Helper.routes_file)
-          content, header_position = Helper.strip_annotations(existing_text)
-          new_content = Helper.strip_on_removal(content, header_position)
-          new_text = new_content.join("\n")
-          if Helper.rewrite_contents(existing_text, new_text)
-            puts "Annotations were removed from #{Helper.routes_file}."
-          else
-            puts "#{Helper.routes_file} was not changed (Annotation did not exist)."
-          end
-        else
-          puts "#{Helper.routes_file} could not be found."
+        routes_file = File.join('config', 'routes.rb')
+        RemovalProcessor.execute(@options, routes_file).tap do |result|
+          puts result
         end
-      end
-
-      def annotate_routes(header, content, header_position, options = {})
-        magic_comments_map, content = Helper.extract_magic_comments_from_array(content)
-        if %w(before top).include?(options[:position_in_routes])
-          header = header << '' if content.first != ''
-          magic_comments_map << '' if magic_comments_map.any?
-          new_content = magic_comments_map + header + content
-        else
-          # Ensure we have adequate trailing newlines at the end of the file to
-          # ensure a blank line separating the content from the annotation.
-          content << '' unless content.last == ''
-
-          # We're moving something from the top of the file to the bottom, so ditch
-          # the spacer we put in the first time around.
-          content.shift if header_position == :before && content.first == ''
-
-          new_content = magic_comments_map + content + header
-        end
-
-        # Make sure we end on a trailing newline.
-        new_content << '' unless new_content.last == ''
-
-        new_content
       end
     end
   end
