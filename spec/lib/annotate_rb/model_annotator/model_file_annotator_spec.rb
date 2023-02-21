075749402880a6603102f51a4573e04dc9861b26
Andrew W. Lee <git@drewlee.com>
2023-02-21 03:21:15 -0800

    Attempt to deal with pollution

--- spec/lib/annotate_rb/model_annotator/model_file_annotator_spec.rb
+++ spec/lib/annotate_rb/model_annotator/model_file_annotator_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe AnnotateRb::ModelAnnotator::ModelFileAnnotator do
   describe '.call' do
     subject do
-      described_class.call([], 'foo.rb', nil, options)
+      described_class.call([], 'some_foo.rb', nil, options)
     end
 
     let(:options) { AnnotateRb::Options.from({ ignore_unknown_models: true }) }
@@ -19,13 +19,13 @@ RSpec.describe AnnotateRb::ModelAnnotator::ModelFileAnnotator do
     end
 
     context 'with a class' do
-      let(:foo_class) do
+      let(:some_foo_class) do
         Class.new(ActiveRecord::Base)
       end
 
       before do
-        allow(AnnotateRb::ModelAnnotator::ModelFilesGetter).to receive(:call).with('foo.rb', options) { foo_class }
-        allow(foo_class).to receive(:table_exists?) { false }
+        allow(AnnotateRb::ModelAnnotator::ModelFilesGetter).to receive(:call).with('some_foo.rb', options) { some_foo_class }
+        allow(some_foo_class).to receive(:table_exists?) { false }
       end
 
       it 'skips attempt to annotate if no table exists for model' do
@@ -37,7 +37,7 @@ RSpec.describe AnnotateRb::ModelAnnotator::ModelFileAnnotator do
     context 'with a non-class' do
       before do
         stub_const('NotAClass', 'foo')
-        allow(AnnotateRb::ModelAnnotator::ModelFilesGetter).to receive(:call).with('foo.rb', options) { NotAClass }
+        allow(AnnotateRb::ModelAnnotator::ModelFilesGetter).to receive(:call).with('some_foo.rb', options) { NotAClass }
       end
 
       it "doesn't output an error" do
