f91be42a1d1ce5e1fbbc301cae0552d012ddacbe
Andrew W. Lee <git@drewlee.com>
2023-02-25 14:15:27 -0800

    Uncomment ActiveRecord::Base monkey patch
    https://github.com/drwl/annotaterb/actions/runs/4258244505/jobs/7409271830

--- lib/annotate_rb/active_record_patch.rb
+++ lib/annotate_rb/active_record_patch.rb
@@ -1,9 +1,9 @@
 # monkey patches
 
-# module ::ActiveRecord
-#   class Base
-#     def self.method_missing(_name, *_args)
-#       # ignore this, so unknown/unloaded macros won't cause parsing to fail
-#     end
-#   end
-# end
+module ::ActiveRecord
+  class Base
+    def self.method_missing(_name, *_args)
+      # ignore this, so unknown/unloaded macros won't cause parsing to fail
+    end
+  end
+end
