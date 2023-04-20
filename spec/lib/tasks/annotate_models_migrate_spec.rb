1f134e938ca6596841f3a18bf0da1a1cf56cad8a
Andrew W. Lee <git@drewlee.com>
2023-04-20 15:37:38 -0700

    Update specs for `annotate_models_migrate_spec.rb`

--- spec/lib/tasks/annotate_models_migrate_spec.rb
+++ spec/lib/tasks/annotate_models_migrate_spec.rb
@@ -12,66 +12,52 @@ RSpec.describe 'ActiveRecord migration rake task hooks' do
       Rake::Task['db:migrate'].invoke
     end
 
-    # Rake::Task.define_task('set_annotation_options')
-    Rake.load_rakefile('tasks/annotate_models_migrate.rake')
+    Rake.load_rakefile('annotate_rb/tasks/annotate_models_migrate.rake')
 
     Rake.application.instance_variable_set(:@top_level_tasks, [subject])
   end
 
   describe 'db:migrate' do
-    it 'should update annotations' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
+    it 'should annotate model files' do
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models'))
       Rake.application.top_level
     end
   end
 
   describe 'db:migrate:up' do
-    it 'should update annotations' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
+    it 'should annotate model files' do
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models'))
       Rake.application.top_level
     end
   end
 
   describe 'db:migrate:down' do
-    it 'should update annotations' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
+    it 'should annotate model files' do
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models'))
       Rake.application.top_level
     end
   end
 
   describe 'db:migrate:reset' do
-    it 'should update annotations' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
+    it 'should annotate model files' do
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models'))
       Rake.application.top_level
     end
   end
 
   describe 'db:rollback' do
-    it 'should update annotations' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
+    it 'should annotate model files' do
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models'))
       Rake.application.top_level
     end
   end
 
   describe 'db:migrate:redo' do
-    it 'should update annotations after all migration tasks' do
-      # TODO: Add expectation
-      # expect(AnnotateRb::Migration).to receive(:update_annotations)
-
-      # Confirm that update_annotations isn't called when the original redo task finishes
-      Rake::Task[subject].enhance do
-        # expect(AnnotateRb::Migration).not_to have_received(:update_annotations)
-      end
+    it 'should annotate model files after all migration tasks' do
+      # Hooked 3 times by db:rollback, db:migrate, and db:migrate:redo tasks
+      expect(AnnotateRb::Runner).to receive(:run).with(a_collection_including('models')).exactly(3).times
 
       Rake.application.top_level
-
-      # Hooked 3 times by db:rollback, db:migrate, and db:migrate:redo tasks
-      # expect(AnnotateRb::Migration).to have_received(:update_annotations).exactly(3).times
     end
   end
 end
