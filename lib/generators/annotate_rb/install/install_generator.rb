5419c9d997b9bcaef5d3efa25b498d33343a3727
Andrew W. Lee <git@drewlee.com>
2023-05-02 13:15:29 -0700

    Structuralize generator install task

---
+++ lib/generators/annotate_rb/install/install_generator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require 'annotate_rb'
+
+module AnnotateRb
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      def copy_task
+        # TODO: copy rake task into Rails app lib/tasks folder
+      end
+    end
+  end
+end
