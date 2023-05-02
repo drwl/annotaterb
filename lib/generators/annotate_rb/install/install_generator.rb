bfe9e068d8dea23810a79532b98c8ffad3b7ea95
Andrew W. Lee <git@drewlee.com>
2023-05-02 13:20:53 -0700

    Change install generator to copy rake task

--- lib/generators/annotate_rb/install/install_generator.rb
+++ lib/generators/annotate_rb/install/install_generator.rb
@@ -4,8 +4,10 @@ require 'annotate_rb'
 module AnnotateRb
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path('templates', __dir__)
+
       def copy_task
-        # TODO: copy rake task into Rails app lib/tasks folder
+        copy_file "auto_annotate_models.rake", "lib/tasks/auto_annotate_models.rake"
       end
     end
   end
