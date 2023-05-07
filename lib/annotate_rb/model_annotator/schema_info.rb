83641e2fdf0420f9e21de94378612f2270e666e7
Andrew W. Lee <git@drewlee.com>
2023-05-07 15:52:15 -0700

    Replace last usages of `column` in `get_attributes`

--- lib/annotate_rb/model_annotator/schema_info.rb
+++ lib/annotate_rb/model_annotator/schema_info.rb
@@ -101,7 +101,7 @@ module AnnotateRb
           column_thing = ColumnThing.new(column, options)
 
           unless column_thing.default.nil? || column_thing.hide_default?
-            attrs << "default(#{schema_default(klass, column)})"
+            attrs << "default(#{schema_default(klass, column_thing)})"
           end
 
           if column_thing.unsigned?
@@ -154,11 +154,12 @@ module AnnotateRb
           # Check if the column has indices and print "indexed" if true
           # If the index includes another column, print it too.
           if options[:simple_indexes] && klass.table_exists? # Check out if this column is indexed
-            indices = retrieve_indexes_from_table(klass).select { |ind| ind.columns.include? column.name }
+            table_indices = retrieve_indexes_from_table(klass)
+            indices = table_indices.select { |ind| ind.columns.include? column_thing.name }
             indices&.sort_by(&:name)&.each do |ind|
               next if ind.columns.is_a?(String)
 
-              ind = ind.columns.reject! { |i| i == column.name }
+              ind = ind.columns.reject! { |i| i == column_thing.name }
               attrs << (ind.empty? ? 'indexed' : "indexed => [#{ind.join(', ')}]")
             end
           end
