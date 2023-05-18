d1425884e1a5e0ae0ae2c4adfc566180a3ddaa12
Andrew W. Lee <git@drewlee.com>
2023-05-17 20:35:06 -0700

    Add test for overwriting a model with column comment

--- spec/lib/annotate_rb/model_annotator/file_annotator_spec.rb
+++ spec/lib/annotate_rb/model_annotator/file_annotator_spec.rb
@@ -97,5 +97,68 @@ RSpec.describe AnnotateRb::ModelAnnotator::FileAnnotator do
         expect(File.read(@model_file_name)).to eq(expected_file_content)
       end
     end
+
+    describe 'annotating a file with existing column comments' do
+      let(:options) { AnnotateRb::Options.from({ with_comment: true }) }
+      let(:schema_info) do
+        <<~SCHEMA
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #
+        SCHEMA
+      end
+      let(:starting_file_content) do
+        <<~FILE
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                       :integer          not null, primary key
+          #  name([sensitivity: low]) :string(50)       not null
+          #
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      before do
+        @model_dir = Dir.mktmpdir('annotaterb')
+        (@model_file_name, _file_content) = write_model('user.rb', starting_file_content)
+
+        @klass = mock_class(:users,
+                            :id,
+                            [
+                              # Having `comment: nil` for id column is the "correct" test setup
+                              # Only MySQL and PostgreSQL adapters support comments AND ModelWrapper#with_comments?
+                              # expects the first column to respond to `comment` method before checking the rest.
+                              mock_column(:id, :integer, comment: nil),
+                              mock_column(:name, :string, limit: 50, comment: '[sensitivity: medium]')
+                            ])
+        # @schema_info = AnnotateRb::ModelAnnotator::AnnotationGenerator.new(@klass, options).generate
+        # puts @schema_info
+      end
+
+      it 'updates the annotations' do
+        AnnotateRb::ModelAnnotator::FileAnnotator.call(@model_file_name, schema_info, :position_in_class, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
   end
 end
