78abe8f525f091bdbf51990ffeb9f2dbebfdf4d5
Andrew W. Lee <git@drewlee.com>
2023-05-18 19:02:40 -0700

    Extract `#regenerate_annotations`

--- lib/annotate_rb/model_annotator/annotated_file_parser.rb
+++ lib/annotate_rb/model_annotator/annotated_file_parser.rb
@@ -6,9 +6,10 @@ module AnnotateRb
     class AnnotatedFileParser
       SKIP_ANNOTATION_STRING = '# -*- SkipSchemaAnnotations'
 
-      def initialize(file_content, new_annotations, options)
+      def initialize(file_content, new_annotations, annotation_position, options)
         @file_content = file_content
         @new_annotations = new_annotations
+        @annotation_position = annotation_position
         @options = options
       end
 
@@ -23,6 +24,27 @@ module AnnotateRb
         @annotation_pattern = AnnotationPatternGenerator.call(@options)
 
         @old_annotations_v1 = @file_content.match(@annotation_pattern).to_s
+      end
+
+      # Used when overwriting existing annotations OR model file has no annotations
+      def regenerate_annotations
+        magic_comments_block = Helper.magic_comments_as_string(@file_content)
+
+        old_content = @file_content.gsub(Constants::MAGIC_COMMENT_MATCHER, '')
+        old_content = old_content.sub(@annotation_pattern, '')
+
+        # Need to keep `.to_s` for now since the it can be either a String or Symbol
+        annotation_write_position = @options[@annotation_position].to_s
+
+        if %w(after bottom).include?(annotation_write_position)
+          new_content = magic_comments_block + (old_content.rstrip + "\n\n" + @new_wrapped_annotations)
+        elsif magic_comments_block.empty?
+          new_content = magic_comments_block + @new_wrapped_annotations + old_content.lstrip
+        else
+          new_content = magic_comments_block + "\n" + @new_wrapped_annotations + old_content.lstrip
+        end
+
+        new_content
       end
 
       def new_wrapped_annotations
--- lib/annotate_rb/model_annotator/file_annotator.rb
+++ lib/annotate_rb/model_annotator/file_annotator.rb
@@ -25,7 +25,7 @@ module AnnotateRb
           return false unless File.exist?(file_name)
           old_content = File.read(file_name)
 
-          parsed_file = AnnotatedFileParser.new(old_content, info_block, options)
+          parsed_file = AnnotatedFileParser.new(old_content, info_block, position, options)
           parsed_file.parse
 
           return false if parsed_file.skip?
@@ -34,28 +34,15 @@ module AnnotateRb
           abort "AnnotateRb error. #{file_name} needs to be updated, but annotaterb was run with `--frozen`." if options[:frozen]
 
           if parsed_file.old_annotations_v1.empty? || options[:force]
-            magic_comments_block = Helper.magic_comments_as_string(old_content)
-            old_content = old_content.gsub(Constants::MAGIC_COMMENT_MATCHER, '')
-            old_content = old_content.sub(parsed_file.annotation_pattern, '')
-
-            new_content = if %w(after bottom).include?(options[position].to_s)
-                            magic_comments_block + (old_content.rstrip + "\n\n" + parsed_file.new_wrapped_annotations)
-                          elsif magic_comments_block.empty?
-                            magic_comments_block + parsed_file.new_wrapped_annotations + old_content.lstrip
-                          else
-                            magic_comments_block + "\n" + parsed_file.new_wrapped_annotations + old_content.lstrip
-                          end
+            updated_file_content = parsed_file.regenerate_annotations
           else
-            # replace the old annotation with the new one
 
-            # keep the surrounding whitespace the same
             space_match = parsed_file.old_annotations_v1.match(/\A(?<start>\s*).*?\n(?<end>\s*)\z/m)
             new_annotation = space_match[:start] + parsed_file.new_wrapped_annotations + space_match[:end]
 
-            new_content = old_content.sub(parsed_file.annotation_pattern, new_annotation)
+            updated_file_content = old_content.sub(parsed_file.annotation_pattern, new_annotation)
           end
 
-          updated_file_content = new_content
           File.open(file_name, 'wb') { |f| f.puts updated_file_content }
           true
         end
