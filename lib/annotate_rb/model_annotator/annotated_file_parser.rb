d94727ea7ed558077c9e1cb9d9712a19841a835e
Andrew W. Lee <git@drewlee.com>
2023-05-20 16:14:13 -0700

    Move `.wrapped_content` out of `Helper`

--- lib/annotate_rb/model_annotator/annotated_file_parser.rb
+++ lib/annotate_rb/model_annotator/annotated_file_parser.rb
@@ -20,7 +20,7 @@ module AnnotateRb
         diff = AnnotationDiffGenerator.new(@file_content, @new_annotations).generate
         @annotations_changed = diff.changed?
 
-        @new_wrapped_annotations = Helper.wrapped_content(@new_annotations, @options)
+        @new_wrapped_annotations = wrapped_content(@new_annotations, @options)
 
         @annotation_pattern = AnnotationPatternGenerator.call(@options)
 
@@ -98,6 +98,24 @@ module AnnotateRb
         new_content = @file_content.sub(@annotation_pattern, new_annotation)
 
         new_content
+      end
+
+      private
+
+      def wrapped_content(content, options)
+        if options[:wrapper_open]
+          wrapper_open = "# #{options[:wrapper_open]}\n"
+        else
+          wrapper_open = ""
+        end
+
+        if options[:wrapper_close]
+          wrapper_close = "# #{options[:wrapper_close]}\n"
+        else
+          wrapper_close = ""
+        end
+
+        _wrapped_info_block = "#{wrapper_open}#{content}#{wrapper_close}"
       end
     end
   end
