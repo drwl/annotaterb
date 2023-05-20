26fcd4d32253425cae3140f5cad266be689fa32d
Andrew W. Lee <git@drewlee.com>
2023-05-20 15:16:42 -0700

    Add test for normal comment

--- spec/lib/annotate_rb/model_annotator/magic_comment_parser_spec.rb
+++ spec/lib/annotate_rb/model_annotator/magic_comment_parser_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe AnnotateRb::ModelAnnotator::MagicCommentParser do
       it { is_expected.to be_blank }
     end
 
+    context 'model file with a normal comment' do
+      let(:content) do
+        <<~FILE
+          # some comment describing the class
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to be_blank }
+    end
+
     context "model file with '# encoding: UTF-8' magic comment" do
       let(:magic_comment) { '# encoding: UTF-8' }
       let(:expected) { "#{magic_comment}\n" }
