f9f69402005c7ae0b8512cb8d9d88317fe52a151
Andrew W. Lee <git@drewlee.com>
2023-05-20 15:09:17 -0700

    Add the rest of magic comment test cases

--- spec/lib/annotate_rb/model_annotator/magic_comment_parser_spec.rb
+++ spec/lib/annotate_rb/model_annotator/magic_comment_parser_spec.rb
@@ -14,5 +14,159 @@ RSpec.describe AnnotateRb::ModelAnnotator::MagicCommentParser do
 
       it { is_expected.to be_blank }
     end
+
+    context "model file with '# encoding: UTF-8' magic comment" do
+      let(:magic_comment) { '# encoding: UTF-8' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# coding: UTF-8' magic comment" do
+      let(:magic_comment) { '# coding: UTF-8' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# -*- coding: UTF-8 -*-' magic comment" do
+      let(:magic_comment) { '# -*- coding: UTF-8 -*-' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '#encoding: utf-8' magic comment" do
+      let(:magic_comment) { '#encoding: utf-8' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# encoding: utf-8' magic comment" do
+      let(:magic_comment) { '# encoding: utf-8' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# -*- encoding : utf-8 -*-' magic comment" do
+      let(:magic_comment) { '# -*- encoding : utf-8 -*-' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with \"# encoding: utf-8\n# frozen_string_literal: true\" magic comment" do
+      let(:magic_comment) { "# encoding: utf-8\n# frozen_string_literal: true" }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with \"# frozen_string_literal: true\n# encoding: utf-8\" magic comment" do
+      let(:magic_comment) { "# frozen_string_literal: true\n# encoding: utf-8" }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# frozen_string_literal: true' magic comment" do
+      let(:magic_comment) { '# frozen_string_literal: true' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '#frozen_string_literal: false' magic comment" do
+      let(:magic_comment) { '#frozen_string_literal: false' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
+
+    context "model file with '# -*- frozen_string_literal : true -*-' magic comment" do
+      let(:magic_comment) { '# -*- frozen_string_literal : true -*-' }
+      let(:expected) { "#{magic_comment}\n" }
+      let(:content) do
+        <<~FILE
+          #{magic_comment}
+          class User < ApplicationRecord
+          end
+        FILE
+      end
+
+      it { is_expected.to eq(expected) }
+    end
   end
 end
