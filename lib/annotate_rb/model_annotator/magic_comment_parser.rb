3a16b095ff25a00fbb8a6fde2ff8c7ff7bfa200d
Andrew W. Lee <git@drewlee.com>
2023-05-20 15:40:34 -0700

    Break down magic comment regex to components

--- lib/annotate_rb/model_annotator/magic_comment_parser.rb
+++ lib/annotate_rb/model_annotator/magic_comment_parser.rb
@@ -4,9 +4,20 @@ module AnnotateRb
   module ModelAnnotator
     # Extracts magic comments strings and returns them
     class MagicCommentParser
+      MAGIC_COMMENTS = [
+        HASH_ENCODING = Regexp.new(/(^#\s*encoding:.*(?:\n|r\n))/),
+        HASH_CODING = Regexp.new(/(^# coding:.*(?:\n|\r\n))/),
+        HASH_FROZEN_STRING = Regexp.new(/(^#\s*frozen_string_literal:.+(?:\n|\r\n))/),
+        STAR_ENCODING = Regexp.new(/(^# -\*- encoding\s?:.*(?:\n|\r\n))/),
+        STAR_CODING = Regexp.new(/(^# -\*- coding:.*(?:\n|\r\n))/),
+        STAR_FROZEN_STRING = Regexp.new(/(^# -\*- frozen_string_literal\s*:.+-\*-(?:\n|\r\n))/)
+      ].freeze
+
+      MAGIC_COMMENTS_REGEX = Regexp.union(*MAGIC_COMMENTS)
+
       class << self
         def call(content)
-          magic_comments = content.scan(Constants::MAGIC_COMMENT_MATCHER).flatten.compact
+          magic_comments = content.scan(MAGIC_COMMENTS_REGEX).flatten.compact
 
           if magic_comments.any?
             magic_comments.join
