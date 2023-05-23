de833bf697f7f7c30c36ae212ae466f114a41563
Andrew W. Lee <git@drewlee.com>
2023-05-22 19:33:37 -0700

    Move class methods into instance methods

--- lib/annotate_rb/model_annotator/model_file_annotator.rb
+++ lib/annotate_rb/model_annotator/model_file_annotator.rb
@@ -6,49 +6,59 @@ module AnnotateRb
     class ModelFileAnnotator
       class << self
         def call(annotated, file, options)
-          begin
-            klass = ModelClassGetter.call(file, options)
-
-            instructions = build_instructions(klass, file, options)
-            instructions.each do |instruction|
-              if FileAnnotator.call_with_instructions(instruction)
-                annotated << instruction.file
-              end
-            end
-
-            annotated
-          rescue BadModelFileError => e
-            unless options[:ignore_unknown_models]
-              $stderr.puts "Unable to annotate #{file}: #{e.message}"
-              $stderr.puts "\t" + e.backtrace.join("\n\t") if options[:trace]
-            end
-          rescue StandardError => e
-            $stderr.puts "Unable to annotate #{file}: #{e.message}"
-            $stderr.puts "\t" + e.backtrace.join("\n\t") if options[:trace]
-          end
+          new(annotated, file, options).annotate
         end
+      end
 
-        private
+      def initialize(annotated, file, options)
+        @annotated = annotated
+        @file = file
+        @options = options
+      end
 
-        def build_instructions(klass, file, options = {})
-          instructions = []
-
-          klass.reset_column_information
-          annotation = AnnotationBuilder.new(klass, options).build
-          model_name = klass.name.underscore
-          table_name = klass.table_name
-
-          model_instruction = FileAnnotatorInstruction.new(file, annotation, :position_in_class, options)
-          instructions << model_instruction
-
-          related_files = RelatedFilesListBuilder.new(file, model_name, table_name, options).build
-          related_file_instructions = related_files.map do |f, position_key|
-            _instruction = FileAnnotatorInstruction.new(f, annotation, position_key, options)
+      def annotate
+        begin
+          instructions = build_instructions
+          instructions.each do |instruction|
+            if FileAnnotator.call_with_instructions(instruction)
+              @annotated << instruction.file
+            end
           end
-          instructions.concat(related_file_instructions)
 
-          instructions
+          @annotated
+        rescue BadModelFileError => e
+          unless @options[:ignore_unknown_models]
+            $stderr.puts "Unable to annotate #{@file}: #{e.message}"
+            $stderr.puts "\t" + e.backtrace.join("\n\t") if @options[:trace]
+          end
+        rescue StandardError => e
+          $stderr.puts "Unable to annotate #{@file}: #{e.message}"
+          $stderr.puts "\t" + e.backtrace.join("\n\t") if @options[:trace]
         end
+      end
+
+      private
+
+      def build_instructions
+        klass = ModelClassGetter.call(@file, @options)
+
+        instructions = []
+
+        klass.reset_column_information
+        annotation = AnnotationBuilder.new(klass, @options).build
+        model_name = klass.name.underscore
+        table_name = klass.table_name
+
+        model_instruction = FileAnnotatorInstruction.new(@file, annotation, :position_in_class, @options)
+        instructions << model_instruction
+
+        related_files = RelatedFilesListBuilder.new(@file, model_name, table_name, @options).build
+        related_file_instructions = related_files.map do |f, position_key|
+          _instruction = FileAnnotatorInstruction.new(f, annotation, position_key, @options)
+        end
+        instructions.concat(related_file_instructions)
+
+        instructions
       end
     end
   end
