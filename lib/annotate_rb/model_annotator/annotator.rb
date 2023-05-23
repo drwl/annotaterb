afcd28d2ad6683ab37c13fd9f06608c6118de792
Andrew W. Lee <git@drewlee.com>
2023-05-22 19:30:04 -0700

    Move class methods into instance methods

--- lib/annotate_rb/model_annotator/annotator.rb
+++ lib/annotate_rb/model_annotator/annotator.rb
@@ -5,60 +5,72 @@ module AnnotateRb
     class Annotator
       class << self
         def do_annotations(options = {})
-          annotated = []
-
-          model_files_to_consider = ModelFilesGetter.call(options)
-
-          model_files_to_consider.each do |path, filename|
-            file = File.join(path, filename)
-
-            if AnnotationDecider.new(file, options).annotate?
-              ModelFileAnnotator.call(annotated, file, options)
-            end
-          end
-
-          if annotated.empty?
-            puts 'Model files unchanged.'
-          else
-            puts "Annotated (#{annotated.length}): #{annotated.join(', ')}"
-          end
+          new(options).do_annotations
         end
 
         def remove_annotations(options = {})
-          unannotated = []
+          new(options).remove_annotations
+        end
+      end
 
-          model_files_to_consider = ModelFilesGetter.call(options)
+      def initialize(options)
+        @options = options
+      end
 
-          model_files_to_consider.each do |path, filename|
-            unannotated_klass = false
-            file = File.join(path, filename)
+      def do_annotations
+        annotated = []
 
-            begin
-              klass = ModelClassGetter.call(file, options)
+        model_files_to_consider = ModelFilesGetter.call(@options)
 
-              if AnnotationDecider.new(file, options).annotate?
-                if FileAnnotationRemover.call(file, options)
-                  unannotated_klass = true
-                end
+        model_files_to_consider.each do |path, filename|
+          file = File.join(path, filename)
 
-                related_files = RelatedFilesListBuilder.new(file, model_name, table_name, options).build
+          if AnnotationDecider.new(file, @options).annotate?
+            ModelFileAnnotator.call(annotated, file, @options)
+          end
+        end
 
-                related_files.each do |f, _position_key|
-                  if File.exist?(f)
-                    FileAnnotationRemover.call(f, options)
-                  end
-                end
+        if annotated.empty?
+          puts 'Model files unchanged.'
+        else
+          puts "Annotated (#{annotated.length}): #{annotated.join(', ')}"
+        end
+      end
+
+      def remove_annotations
+        unannotated = []
+
+        model_files_to_consider = ModelFilesGetter.call(@options)
+
+        model_files_to_consider.each do |path, filename|
+          unannotated_klass = false
+          file = File.join(path, filename)
+
+          begin
+            klass = ModelClassGetter.call(file, @options)
+
+            if AnnotationDecider.new(file, @options).annotate?
+              if FileAnnotationRemover.call(file, @options)
+                unannotated_klass = true
               end
 
-              unannotated << klass if unannotated_klass
-            rescue StandardError => e
-              $stderr.puts "Unable to unannotate #{File.join(file)}: #{e.message}"
-              $stderr.puts "\t" + e.backtrace.join("\n\t") if options[:trace]
-            end
-          end
+              related_files = RelatedFilesListBuilder.new(file, model_name, table_name, @options).build
 
-          puts "Removed annotations from: #{unannotated.join(', ')}"
+              related_files.each do |f, _position_key|
+                if File.exist?(f)
+                  FileAnnotationRemover.call(f, @options)
+                end
+              end
+            end
+
+            unannotated << klass if unannotated_klass
+          rescue StandardError => e
+            $stderr.puts "Unable to unannotate #{File.join(file)}: #{e.message}"
+            $stderr.puts "\t" + e.backtrace.join("\n\t") if @options[:trace]
+          end
         end
+
+        puts "Removed annotations from: #{unannotated.join(', ')}"
       end
     end
   end
