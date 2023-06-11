546208dcc5c3b3ac2c69f87b6ee13143ca71bfb6
Andrew W. Lee <git@drewlee.com>
2023-06-11 14:46:11 -0700

    Add another spec

--- spec/integration/cli.rb
+++ spec/integration/cli.rb
@@ -9,9 +9,23 @@ RSpec.describe "CLI", type: "aruba" do
 
     let(:error_message) { "Please run annotaterb from the root of the project." }
 
-    it do
+    it "exits and outputs an error message" do
       expect(last_command_started).to have_exit_status(1)
       expect(last_command_started).to have_output_on_stderr(error_message)
+    end
+  end
+
+  context "when running in a directory with a Rakefile and a Gemfile" do
+    before do
+      touch("Rakefile", "Gemfile")
+      run_command("annotaterb")
+    end
+
+    let(:help_banner_fragment) { "Usage: annotaterb [command] [options]" }
+
+    it "outputs the help message" do
+      expect(last_command_started).to be_successfully_executed
+      expect(last_command_started.stdout).to include(help_banner_fragment)
     end
   end
 end
