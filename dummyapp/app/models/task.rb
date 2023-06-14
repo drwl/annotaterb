f6ceffbb2a2d7b11a4f71ca944cb5c9cbbc66fdc
Andrew W. Lee <git@drewlee.com>
2023-06-14 15:08:00 -0700

    Add more annotation cases to dummyapp

--- dummyapp/app/models/task.rb
+++ dummyapp/app/models/task.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  content    :string
+#  count      :integer
+#  status     :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Task < ApplicationRecord
 end
