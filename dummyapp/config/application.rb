eba215fee4a0675153e9cb778ad1e460d6e39b43
Andrew W. Lee <git@drewlee.com>
2023-09-07 19:48:22 -0700

    Run integration tests in CI

--- dummyapp/config/application.rb
+++ dummyapp/config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Dummyapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
 
     # Configuration for the application, engines, and railties goes here.
     #
