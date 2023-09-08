93767b54518b429c5f142f40480154777c9f67c3
Andrew W. Lee <git@drewlee.com>
2023-09-07 19:34:23 -0700

    Revert "Update template directory in spec"
    This reverts commit cd066ae8cceb829fe1337583cf837aea9ca6606e.

--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -6,9 +6,3 @@ require "aruba/rspec"
 Aruba.configure do |config|
   config.allow_absolute_paths = true
 end
-
-module IntegrationTestHelper
-  def self.ruby_version
-    RUBY_VERSION.split(".").first(2).join("_") # => "3_0"
-  end
-end
