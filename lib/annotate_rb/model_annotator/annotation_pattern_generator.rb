08383a875372f407dde0846d3b44b7b92de78de4
Andrew W. Lee <git@drewlee.com>
2023-09-23 20:12:03 -0500

    Remove default value from `options` args

--- lib/annotate_rb/model_annotator/annotation_pattern_generator.rb
+++ lib/annotate_rb/model_annotator/annotation_pattern_generator.rb
@@ -7,7 +7,7 @@ module AnnotateRb
       COMPAT_PREFIX_MD = "## Schema Info"
 
       class << self
-        def call(options = Options.from({}))
+        def call(options)
           if options[:wrapper_open]
             return /(?:^(\n|\r\n)?# (?:#{options[:wrapper_open]}).*(\n|\r\n)?# (?:#{COMPAT_PREFIX}|#{COMPAT_PREFIX_MD}).*?(\n|\r\n)(#.*(\n|\r\n))*(\n|\r\n)*)|^(\n|\r\n)?# (?:#{COMPAT_PREFIX}|#{COMPAT_PREFIX_MD}).*?(\n|\r\n)(#.*(\n|\r\n))*(\n|\r\n)*/
           end
