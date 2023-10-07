f0031cb84abff61c7dcb8fd5358730b03208c74c
Andrew W. Lee <git@drewlee.com>
2023-10-06 17:02:56 -0700

    Add integration test for annotating routes (#68)
    This PR adds an integration for annotating routes in a dummy Rails app.

--- spec/dummyapp/config/routes.rb
+++ spec/dummyapp/config/routes.rb
@@ -1,6 +1,9 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root "articles#index"
+  resources :resources
+  resource :singular_resource
+
+  get "/manual", to: "manual#show"
 end
---
+++ spec/integration/annotate_routes_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "integration_spec_helper"
+
+RSpec.describe "Annotate routes", type: "aruba" do
+  let(:command_timeout_seconds) { 10 }
+
+  before do
+    copy(Dir[File.join(aruba.config.root_directory, "spec/dummyapp/*")], aruba.config.home_directory)
+
+    # Unset the bundler context from running annotaterb integration specs.
+    #   This way, when `run_command("bundle exec annotaterb")` runs, it runs as if it's within the context of dummyapp.
+    unset_bundler_env_vars
+  end
+
+  let(:templates_dir) { File.join(aruba.config.root_directory, "spec/templates/") }
+  let(:routes_file) { "config/routes.rb" }
+
+  it "annotates a single file" do
+    expected_routes_file = read(File.join(templates_dir, "routes.rb")).join("\n")
+    original_routes_file = read(routes_file).join("\n")
+
+    # Check that files have been copied over correctly
+    expect(expected_routes_file).not_to eq(original_routes_file)
+
+    _cmd = run_command_and_stop("bundle exec annotaterb routes", fail_on_error: true, exit_timeout: command_timeout_seconds)
+
+    annotated_routes_file = read(routes_file).join("\n")
+
+    expect(last_command_started).to be_successfully_executed
+    expect(annotated_routes_file).to eq(expected_routes_file)
+  end
+end
