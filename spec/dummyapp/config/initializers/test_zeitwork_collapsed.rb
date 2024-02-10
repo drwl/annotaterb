ef6162228561dc63abdd235d89b2e7b3df88ecd0
Andrew W. Lee <git@drewlee.com>
2024-02-11 00:31:12 +0900

    Add in the remainder code

--- spec/dummyapp/config/initializers/test_zeitwork_collapsed.rb
+++ spec/dummyapp/config/initializers/test_zeitwork_collapsed.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-Rails.autoloaders.main.collapse("#{Rails.root}/app/models/collapsed/example")
+collapsed_example = "#{Rails.root}/app/models/collapsed/example"
+Rails.autoloaders.main.collapse(collapsed_example)
+
+unless Rails.application.config.eager_load
+  Rails.application.config.to_prepare do
+    Rails.autoloaders.main.eager_load_dir(collapsed_example)
+  end
+end
