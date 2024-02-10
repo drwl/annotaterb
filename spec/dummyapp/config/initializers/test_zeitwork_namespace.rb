fe6844fc03c591a03b437253c6de7b31278cfe3f
Andrew W. Lee <git@drewlee.com>
2024-02-10 19:14:44 +0900

    Lint file

--- spec/dummyapp/config/initializers/test_zeitwork_namespace.rb
+++ spec/dummyapp/config/initializers/test_zeitwork_namespace.rb
@@ -1,3 +1,5 @@
-Object.const_set('Namespace', Module.new) unless Object.const_defined?('Namespace')
+# frozen_string_literal: true
+
+Object.const_set(:Namespace, Module.new) unless Object.const_defined?(:Namespace)
 
 Rails.autoloaders.main.push_dir(Rails.root.join("app/namespace/models"), namespace: Namespace)
