44047b7b2c0dc1a360034ce5090a75479fc23ccd
Andrew W. Lee <git@drewlee.com>
2024-02-11 16:54:24 +0900

    Improve tests in `annotate_after_migration_spec`

--- spec/integration/annotate_after_migration_spec.rb
+++ spec/integration/annotate_after_migration_spec.rb
@@ -13,15 +13,6 @@ RSpec.describe "Annotate after running migrations", type: "aruba" do
     unset_bundler_env_vars
   end
 
-  after do
-    # Rollback copied migration to avoid test pollution (for mysql and postgres)
-    _cmd = run_command_and_stop("bin/rails db:migrate:down VERSION=20231013230731", fail_on_error: true, exit_timeout: command_timeout_seconds)
-
-    dummy_app_migration_file = File.join("db/migrate", migration_file)
-    # TODO: Look into Aruba cleaning the tmp working directory between tests since we copy the dummyapp every test
-    remove(dummy_app_migration_file)
-  end
-
   let(:templates_dir) { File.join(aruba.config.root_directory, "spec/templates/#{ENV["DATABASE_ADAPTER"]}") }
   let(:migrations_templates_dir) { File.join(aruba.config.root_directory, "spec/templates/migrations") }
   let(:migration_file) { "20231013230731_add_int_field_to_test_defaults.rb" }
@@ -29,6 +20,12 @@ RSpec.describe "Annotate after running migrations", type: "aruba" do
   let(:models_dir) { "app/models" }
 
   it "adds annotations for the new field" do
+    reset_database
+    run_migrations
+
+    # Start with the already annotated TestDefault model
+    copy(File.join(templates_dir, "test_default.rb"), models_dir)
+
     expected_test_default = read(File.join(templates_dir, "test_default_updated.rb")).join("\n")
     original_test_default = read(File.join(models_dir, "test_default.rb")).join("\n")
 
@@ -51,12 +48,9 @@ RSpec.describe "Annotate after running migrations", type: "aruba" do
       _cmd = run_command_and_stop("bin/rails g annotate_rb:install", fail_on_error: true, exit_timeout: command_timeout_seconds)
     end
 
-    after do
-      remove("lib/tasks/annotate_rb.rake")
-      remove(".annotaterb.yml")
-    end
-
     it "annotations are automatically added during migration" do
+      reset_database
+
       expected_test_default = read(File.join(templates_dir, "test_default_updated.rb")).join("\n")
       original_test_default = read(File.join(models_dir, "test_default.rb")).join("\n")
 
