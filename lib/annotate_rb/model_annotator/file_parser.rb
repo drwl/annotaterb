ebadd16e58b779b73086ea1f76007fc2f67b4c56
Andrew W. Lee <git@drewlee.com>
2024-02-18 00:55:07 +0900

    Remove `MagicCommentParser` as it is no longer used

--- lib/annotate_rb/model_annotator/file_parser.rb
+++ lib/annotate_rb/model_annotator/file_parser.rb
@@ -3,7 +3,6 @@
 module AnnotateRb
   module ModelAnnotator
     module FileParser
-      autoload :MagicCommentParser, "annotate_rb/model_annotator/file_parser/magic_comment_parser"
       autoload :AnnotationFinder, "annotate_rb/model_annotator/file_parser/annotation_finder"
       autoload :CustomParser, "annotate_rb/model_annotator/file_parser/custom_parser"
       autoload :ParsedFile, "annotate_rb/model_annotator/file_parser/parsed_file"
