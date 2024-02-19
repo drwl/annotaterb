1d2041808f51a0aff0ffe7ba3ebb258f7d189830
Andrew W. Lee <git@drewlee.com>
2024-02-19 13:57:51 +0900

    Extract code into methods

--- lib/annotate_rb/model_annotator/annotated_file/generator.rb
+++ lib/annotate_rb/model_annotator/annotated_file/generator.rb
@@ -33,10 +33,21 @@ module AnnotateRb
           # We need to get class start and class end depending on the position
           parsed = FileParser::CustomParser.new(content_without_annotations, "", 0).tap(&:parse)
 
-          same_write_position = @parsed_file.has_annotations? && @parsed_file.annotation_position.to_s == annotation_write_position
+          _content = if %w[after bottom].include?(annotation_write_position)
+            content_annotated_after(parsed, content_without_annotations)
+          else
+            content_annotated_before(parsed, content_without_annotations, annotation_write_position)
+          end
+        end
+
+        private
+
+        def content_annotated_before(parsed, content_without_annotations, write_position)
+          same_write_position = @parsed_file.has_annotations? && @parsed_file.annotation_position.to_s == write_position
 
           # Could error if there's no class or module declaration
           _constant_name, line_number_before = parsed.starts.first
+
           content_with_annotations_written_before = []
           content_with_annotations_written_before << content_without_annotations.lines[0...line_number_before]
           content_with_annotations_written_before << $/ if @parsed_file.has_leading_whitespace? && same_write_position
@@ -44,21 +55,20 @@ module AnnotateRb
           content_with_annotations_written_before << $/ if @parsed_file.has_trailing_whitespace? && same_write_position
           content_with_annotations_written_before << content_without_annotations.lines[line_number_before..]
 
+          content_with_annotations_written_before.join
+        end
+
+        def content_annotated_after(parsed, content_without_annotations)
           _constant_name, line_number_after = parsed.ends.last
+
           content_with_annotations_written_after = []
           content_with_annotations_written_after << content_without_annotations.lines[0..line_number_after]
           content_with_annotations_written_after << $/
           content_with_annotations_written_after << @new_wrapped_annotations.lines
           content_with_annotations_written_after << content_without_annotations.lines[(line_number_after + 1)..]
 
-          _content = if %w[after bottom].include?(annotation_write_position)
-            content_with_annotations_written_after.join
-          else
-            content_with_annotations_written_before.join
-          end
+          content_with_annotations_written_after.join
         end
-
-        private
 
         def wrapped_content(content)
           wrapper_open = if @options[:wrapper_open]
