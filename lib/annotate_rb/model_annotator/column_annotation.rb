f8e1780c84d9e98335b47d4df9a29cfacb0f9157
Andrew W. Lee <git@drewlee.com>
2024-07-07 17:39:28 -0700

    Extract Column component

--- lib/annotate_rb/model_annotator/column_annotation.rb
+++ lib/annotate_rb/model_annotator/column_annotation.rb
@@ -8,6 +8,7 @@ module AnnotateRb
       autoload :ColumnWrapper, "annotate_rb/model_annotator/column_annotation/column_wrapper"
       autoload :AnnotationBuilder, "annotate_rb/model_annotator/column_annotation/annotation_builder"
       autoload :DefaultValueBuilder, "annotate_rb/model_annotator/column_annotation/default_value_builder"
+      autoload :ColumnComponent, "annotate_rb/model_annotator/column_annotation/column_component"
     end
   end
 end
