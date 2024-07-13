b32fc52cacbb2e5363ef827c2556057001d1bc1c
Andrew W. Lee <git@drewlee.com>
2024-07-12 19:15:00 -0700

    Break down tests into smaller tests

--- spec/lib/annotate_rb/model_annotator/foreign_key_annotation/annotation_builder_spec.rb
+++ spec/lib/annotate_rb/model_annotator/foreign_key_annotation/annotation_builder_spec.rb
@@ -78,6 +78,46 @@ RSpec.describe AnnotateRb::ModelAnnotator::ForeignKeyAnnotation::AnnotationBuild
 
         it { expect(markdown_format).to eq(expected_output) }
       end
+
+      context "when show_complete_foreign_keys option is false" do
+        let(:options) { ::AnnotateRb::Options.new({show_foreign_keys: true, show_complete_foreign_keys: false}) }
+
+        let(:expected_output) do
+          <<~OUTPUT.strip
+            #
+            # Foreign Keys
+            #
+            #  custom_fk_name  (other_thing_id => other_things.id)
+            #  fk_rails_...    (foreign_thing_id => foreign_things.id)
+            #  fk_rails_...    (third_thing_id => third_things.id)
+          OUTPUT
+        end
+
+        it { expect(default_format).to eq(expected_output) }
+      end
+    end
+
+    context "with a foreign key with options" do
+      let(:foreign_keys) do
+        [
+          mock_foreign_key("fk_rails_02e851e3b7",
+            "foreign_thing_id",
+            "foreign_things",
+            "id",
+            on_delete: "on_delete_value",
+            on_update: "on_update_value")
+        ]
+      end
+      let(:expected_output) do
+        <<~OUTPUT.strip
+          #
+          # Foreign Keys
+          #
+          #  fk_rails_02e851e3b7  (foreign_thing_id => foreign_things.id) ON DELETE => on_delete_value ON UPDATE => on_update_value
+        OUTPUT
+      end
+
+      it { expect(default_format).to eq(expected_output) }
     end
 
     context "with a composite foreign key" do
