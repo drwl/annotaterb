a8fc4d63754a07c48a5fed54a38532e5aadea63b
Andrew W. Lee <git@drewlee.com>
2024-10-20 18:53:46 -0700

    Update mysql2 and pg templates

--- spec/templates/mysql2/test_child_default.rb
+++ spec/templates/mysql2/test_child_default.rb
@@ -2,10 +2,10 @@
 #
 # Table name: test_child_defaults
 #
-#  id              :integer          not null, primary key
+#  id              :bigint           not null, primary key
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
-#  test_default_id :integer          not null
+#  test_default_id :bigint           not null
 #
 # Indexes
 #
@@ -13,7 +13,7 @@
 #
 # Foreign Keys
 #
-#  test_default_id  (test_default_id => test_defaults.id)
+#  fk_rails_...  (test_default_id => test_defaults.id)
 #
 class TestChildDefault < ApplicationRecord
   belongs_to :test_default
