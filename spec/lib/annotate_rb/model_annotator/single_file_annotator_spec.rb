2d9f516e5f8afbb0185f9d87935cfe10f70d4318
Andrew W. Lee <git@drewlee.com>
2024-10-20 18:28:39 -0700

    Support parsing of dynamic fixture erb yml files (#158)
    This PR adds support for dynamic fixtures, which are yml files with Ruby
    code in them. Rails supports this so we also want to support this.
    
    Before this change, Ruby's yml parser, Psych, would raise
    `Psych::SyntaxError` when attempting to parse non-valid yml. This change
    attempts to parse yml normally, but if it fails will run the input
    through the Erb evaluator and then attempt to parse the output using
    Psych.
    
    Addresses #149

--- spec/lib/annotate_rb/model_annotator/single_file_annotator_spec.rb
+++ spec/lib/annotate_rb/model_annotator/single_file_annotator_spec.rb
@@ -314,5 +314,122 @@ RSpec.describe AnnotateRb::ModelAnnotator::SingleFileAnnotator do
         expect(File.read(@model_file_name)).to eq(expected_file_content)
       end
     end
+
+    describe "annotating a yml file with erb with position before" do
+      let(:options) { AnnotateRb::Options.new({with_comment: true, position_in_fixture: "before"}) }
+      let(:schema_info) do
+        <<~SCHEMA
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #  email                       :string
+          #
+        SCHEMA
+      end
+      let(:starting_file_content) do
+        <<~FILE
+          # Read about fixtures at https://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
+
+          <% 1.upto(100) do |i| %>
+          user_<%= i %>:
+            name: User <%= i %>
+            email: user_<%= i %>@example.com
+          <% end %>
+
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # Read about fixtures at https://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
+
+          <% 1.upto(100) do |i| %>
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #  email                       :string
+          #
+          user_<%= i %>:
+            name: User <%= i %>
+            email: user_<%= i %>@example.com
+          <% end %>
+
+        FILE
+      end
+
+      before do
+        @model_dir = Dir.mktmpdir("annotaterb")
+        (@model_file_name, _file_content) = write_model("user.yml", starting_file_content)
+      end
+
+      it "updates the annotations" do
+        described_class.call(@model_file_name, schema_info, :position_in_fixture, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
+
+    describe "annotating a yml file with erb with position after" do
+      let(:options) { AnnotateRb::Options.new({with_comment: true, position_in_fixture: "after"}) }
+      let(:schema_info) do
+        <<~SCHEMA
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #  email                       :string
+          #
+        SCHEMA
+      end
+      let(:starting_file_content) do
+        <<~FILE
+          # Read about fixtures at https://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
+
+          <% 1.upto(100) do |i| %>
+          user_<%= i %>:
+            name: User <%= i %>
+            email: user_<%= i %>@example.com
+          <% end %>
+
+        FILE
+      end
+      let(:expected_file_content) do
+        <<~FILE
+          # Read about fixtures at https://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
+
+          <% 1.upto(100) do |i| %>
+          user_<%= i %>:
+            name: User <%= i %>
+            email: user_<%= i %>@example.com
+          <% end %>
+
+
+          # == Schema Information
+          #
+          # Table name: users
+          #
+          #  id                          :integer          not null, primary key
+          #  name([sensitivity: medium]) :string(50)       not null
+          #  email                       :string
+          #
+        FILE
+      end
+
+      before do
+        @model_dir = Dir.mktmpdir("annotaterb")
+        (@model_file_name, _file_content) = write_model("user.yml", starting_file_content)
+      end
+
+      it "updates the annotations" do
+        described_class.call(@model_file_name, schema_info, :position_in_fixture, options)
+        expect(File.read(@model_file_name)).to eq(expected_file_content)
+      end
+    end
   end
 end
