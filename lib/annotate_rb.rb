3279c9c87736f7009bb160a7c789af3c1892f1ef
Seth Herr <seth.william.herr@gmail.com>
2025-02-24 11:20:41 -0800

    rename lib

--- lib/annotate_rb.rb
+++ lib/annotate_rb.rb
@@ -1,28 +1,4 @@
-# frozen_string_literal: true
+# Gem names that follow naming convention work seamlessly. Previously this gem name didn't match naming conventions
+# To enable backward compatibility, include this file
 
-require "active_record"
-require "active_support"
-
-# Helper.fallback depends on this being required because it adds #present? to nil
-require "active_support/core_ext/object/blank"
-require "active_support/core_ext/class/subclasses"
-require "active_support/core_ext/string/inflections"
-
-require "rake"
-
-require_relative "annotaterb/helper"
-require_relative "annotaterb/core"
-require_relative "annotaterb/commands"
-require_relative "annotaterb/parser"
-require_relative "annotaterb/runner"
-require_relative "annotaterb/route_annotator"
-require_relative "annotaterb/model_annotator"
-require_relative "annotaterb/options"
-require_relative "annotaterb/eager_loader"
-require_relative "annotaterb/rake_bootstrapper"
-require_relative "annotaterb/config_finder"
-require_relative "annotaterb/config_loader"
-require_relative "annotaterb/config_generator"
-
-module Annotaterb
-end
+require "annotaterb"
--- lib/annotaterb.rb
+++ lib/annotaterb.rb
@@ -1,4 +1,28 @@
-# Gem names that follow naming convention work seamlessly. However, this gem is "annotaterb" where in code it is
-# Annotaterb. Because of this, we need this file so that the rest of the library automatically gets required.
+# frozen_string_literal: true
 
-require "annotaterb"
+require "active_record"
+require "active_support"
+
+# Helper.fallback depends on this being required because it adds #present? to nil
+require "active_support/core_ext/object/blank"
+require "active_support/core_ext/class/subclasses"
+require "active_support/core_ext/string/inflections"
+
+require "rake"
+
+require_relative "annotaterb/helper"
+require_relative "annotaterb/core"
+require_relative "annotaterb/commands"
+require_relative "annotaterb/parser"
+require_relative "annotaterb/runner"
+require_relative "annotaterb/route_annotator"
+require_relative "annotaterb/model_annotator"
+require_relative "annotaterb/options"
+require_relative "annotaterb/eager_loader"
+require_relative "annotaterb/rake_bootstrapper"
+require_relative "annotaterb/config_finder"
+require_relative "annotaterb/config_loader"
+require_relative "annotaterb/config_generator"
+
+module Annotaterb
+end
