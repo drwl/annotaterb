8c2236964a90f5ec842849b330a7c1108bb252ab
Andrew W. Lee <git@drewlee.com>
2025-08-16 18:23:08 -0700

    Revert "Speed up AnnotationDecider (#243)"
    This reverts commit ccd41d2e0073bed8af66c8b90b64bdc3d9c0ad84.

--- lib/annotate_rb/model_annotator/annotation_decider.rb
+++ lib/annotate_rb/model_annotator/annotation_decider.rb
@@ -16,9 +16,29 @@ module AnnotateRb
 
         begin
           klass = ModelClassGetter.call(@file, @options)
-          return false unless klass.respond_to?(:descends_from_active_record?) && klass.descends_from_active_record? && !klass.abstract_class? && klass.table_exists?
 
-          return @options[:exclude_sti_subclasses] ? klass.base_class? : true
+          klass_is_a_class = klass.is_a?(Class)
+          # Methods such as #superclass only exist on a class. Because of how the code is structured, `klass` could be a
+          #  module that does not support the #superclass method, so we want to return early.
+          return false if !klass_is_a_class
+
+          klass_inherits_active_record_base = klass < ActiveRecord::Base
+          klass_is_not_abstract = klass.respond_to?(:abstract_class?) && !klass.abstract_class?
+          klass_table_exists = klass.respond_to?(:table_exists?) && klass.table_exists?
+
+          not_sure_this_conditional = (!@options[:exclude_sti_subclasses] || !(klass.superclass < ActiveRecord::Base && klass.table_name == klass.superclass.table_name))
+
+          annotate_conditions = [
+            klass_is_a_class,
+            klass_inherits_active_record_base,
+            not_sure_this_conditional,
+            klass_is_not_abstract,
+            klass_table_exists
+          ]
+
+          to_annotate = annotate_conditions.all?
+
+          return to_annotate
         rescue BadModelFileError => e
           unless @options[:ignore_unknown_models]
             warn "Unable to process #{@file}: #{e.message}"
