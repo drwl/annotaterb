48c76d2baf183b5cbce82ef49d7fba9e57be5147
Andrew W. Lee <git@drewlee.com>
2025-08-24 17:45:32 -0700

    Add True STI model example

--- spec/integration/annotate_sti_model_spec.rb
+++ spec/integration/annotate_sti_model_spec.rb
@@ -2,7 +2,7 @@
 
 require "integration_spec_helper"
 
-RSpec.describe "Annotate STI model", type: "aruba" do
+RSpec.describe "Annotate STI models", type: "aruba" do
   let(:models_dir) { "app/models" }
   let(:command_timeout_seconds) { 10 }
 
@@ -10,35 +10,47 @@ RSpec.describe "Annotate STI model", type: "aruba" do
     reset_database
     run_migrations
 
-    expected_test_model = read_file(model_template("test_sibling_default.rb"))
+    # Pseudo STI model does not have a `type` field, where the True STI model does.
+    # Both inherit/subclass from another model.
+    expected_pseudo_sti_model = read_file(model_template("test_sibling_default.rb"))
+    original_pseudo_sti_model = read_file(dummyapp_model("test_sibling_default.rb"))
+    expect(expected_pseudo_sti_model).not_to eq(original_pseudo_sti_model)
 
-    original_test_model = read_file(dummyapp_model("test_sibling_default.rb"))
-
-    expect(expected_test_model).not_to eq(original_test_model)
+    expected_true_sti_model = read_file(model_template("test_true_sti.rb"))
+    original_true_sti_model =  read_file(dummyapp_model("test_true_sti.rb"))
+    expect(expected_true_sti_model).not_to eq(original_true_sti_model)
 
     _cmd = run_command_and_stop("bundle exec annotaterb models", fail_on_error: true, exit_timeout: command_timeout_seconds)
 
-    annotated_test_model = read_file(dummyapp_model("test_sibling_default.rb"))
-
     expect(last_command_started).to be_successfully_executed
-    expect(expected_test_model).to eq(annotated_test_model)
+
+    annotated_pseudo_sti_model = read_file(dummyapp_model("test_sibling_default.rb"))
+    expect(expected_pseudo_sti_model).to eq(annotated_pseudo_sti_model)
+
+    annotated_true_sti_model = read_file(dummyapp_model("test_true_sti.rb"))
+    expect(expected_true_sti_model).to eq(annotated_true_sti_model)
   end
 
   it "does not annotate when excluding sti subclasses" do
     reset_database
     run_migrations
 
-    expected_test_model = read_file(model_template("test_sibling_default.rb"))
+    expected_pseudo_sti_model = read_file(model_template("test_sibling_default.rb"))
+    original_pseudo_sti_model = read_file(dummyapp_model("test_sibling_default.rb"))
+    expect(expected_pseudo_sti_model).to_not eq(original_pseudo_sti_model)
 
-    original_test_model = read_file(dummyapp_model("test_sibling_default.rb"))
-
-    expect(expected_test_model).to_not eq(original_test_model)
+    expected_true_sti_model = read_file(model_template("test_true_sti.rb"))
+    original_true_sti_model =  read_file(dummyapp_model("test_true_sti.rb"))
+    expect(expected_true_sti_model).not_to eq(original_true_sti_model)
 
     _cmd = run_command_and_stop("bundle exec annotaterb models --exclude sti_subclasses", fail_on_error: true, exit_timeout: command_timeout_seconds)
 
-    annotated_test_model = read_file(dummyapp_model("test_sibling_default.rb"))
-
     expect(last_command_started).to be_successfully_executed
-    expect(expected_test_model).to_not eq(annotated_test_model)
+
+    annotated_pseudo_sti_model = read_file(dummyapp_model("test_sibling_default.rb"))
+    expect(expected_pseudo_sti_model).to_not eq(annotated_pseudo_sti_model)
+
+    annotated_true_sti_model = read_file(dummyapp_model("test_true_sti.rb"))
+    expect(expected_true_sti_model).to_not eq(annotated_true_sti_model)
   end
 end
