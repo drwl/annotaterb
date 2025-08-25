87bbba019aab756dd85887a9d0f6d4fc50a02f96
Andrew W. Lee <git@drewlee.com>
2025-08-24 20:22:04 -0700

    Remove annotations from models

--- spec/dummyapp/app/models/test_true_sti.rb
+++ spec/dummyapp/app/models/test_true_sti.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
-# == Schema Information
-#
-# Table name: test_parents
-#
-#  id         :integer          not null, primary key
-#  something  :string
-#  type       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
 class TestTrueSti < TestParent
 end
