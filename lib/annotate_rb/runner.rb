be50ec17354082c84161e9b8fc2c9a270dc48797
Ngan Pham <ngan@users.noreply.github.com>
2025-10-22 10:42:19 -0600

    Restore file

--- lib/annotate_rb/runner.rb
+++ lib/annotate_rb/runner.rb
@@ -23,18 +23,23 @@ module AnnotateRb
     end
 
     def run(args)
+      config_file_options = ConfigLoader.load_config
       parser = Parser.new(args, {})
+
       parsed_options = parser.parse
       remaining_args = parser.remaining_args
 
+      options = config_file_options.merge(parsed_options)
+
+      @options = Options.from(options, {working_args: remaining_args})
       AnnotateRb::RakeBootstrapper.call
 
-      options = ConfigLoader.load_config.merge(parsed_options)
-      @options = Options.from(ConfigLoader.load_config.merge(parsed_options), { working_args: remaining_args })
-
-      raise "Didn't specify a command" unless @options[:command]
-
-      @options[:command].call(@options)
+      if @options[:command]
+        @options[:command].call(@options)
+      else
+        # TODO
+        raise "Didn't specify a command"
+      end
     end
   end
 end
