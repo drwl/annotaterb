ae268c8c10fc9831fb082d9904fbbd1f5ff7ecdb
OdenTakashi <odentakashidesu2@gmail.com>
2026-05-31 13:13:58 +0900

    Fix update_config for non-default config locations
    ## Problem
    The update_config generator always targeted the fixed `.annotaterb.yml` path.
    When a project stored its annotaterb config in another supported location,
    running update_config did not update that actual config file.
    
    ## Solution
    Resolve the target config file through ConfigFinder.find_project_dotfile.
    Also honor `--config-path` by setting `ConfigFinder.config_path` before resolving
    the file to update.

--- lib/generators/annotate_rb/update_config/update_config_generator.rb
+++ lib/generators/annotate_rb/update_config/update_config_generator.rb
@@ -6,7 +6,10 @@ module AnnotateRb
   module Generators
     class UpdateConfigGenerator < ::Rails::Generators::Base
       def generate_config
-        insert_into_file ::AnnotateRb::ConfigFinder::DOTFILE do
+        parsed_options = AnnotateRb::Parser.parse(ARGV, {})
+        AnnotateRb::ConfigFinder.config_path = parsed_options[:config_path] if parsed_options[:config_path]
+
+        insert_into_file ::AnnotateRb::ConfigFinder.find_project_dotfile do
           ::AnnotateRb::ConfigGenerator.unset_config_defaults
         end
       end
